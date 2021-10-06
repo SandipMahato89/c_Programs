c30c90d0fcca2b24f21f203dadf69ab15ad7ea05
Sandip Mahato <65347607+SandipMahato89@users.noreply.github.com>
2021-10-06 13:53:28 +0530

    Create product.cpp
    product of two numbers

---
+++ product.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float n1,n2,result;
+    cin>>n1>>n2;
+    result=n1*n2;
+    cout<<"Product of "<<n1<<" and "<<n2<<" is "<<result<<endl;
+    return 0;
+}
